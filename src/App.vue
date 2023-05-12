ad5e994cfe6e49dc94d7770db32aa1ccbf20d9c7
Christopher <Fimbelowski@users.noreply.github.com>
2023-05-11 20:10:42 -0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <div />
+</template>
+
+<style lang="scss">
+@import "./sass/base/reset";
+@import "./sass/base/typography";
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+/// <reference types="vitest" />
+
+import path from "path";
+
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@import "@/sass/abstracts/mixins.scss";',
+      },
+    },
+  },
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  test: {
+    coverage: {
+      reporter: ["text"],
+    },
+    environment: "jsdom",
+    globals: true,
+  },
+});
